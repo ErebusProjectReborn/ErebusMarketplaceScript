a6795b6f958a76c4bbff17abf15ca41ee941dd90
King Erebus <squardisk@gmail.com>
2026-06-21 21:25:12 -0400

    Beta v2.0.0 Update Commit

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,8 +2,8 @@
 
 /*
  * =========================================================================
- * © 2026 Erebus Development Team
- * Author: AnonymousUser9183
+ * © 2026 Erebus Labs Inc.
+ * Author: Czar Erebus
  * =========================================================================
  * Vendor Controller - Vendor Dashboard, Products, Orders, & Advertisements
  * =========================================================================
