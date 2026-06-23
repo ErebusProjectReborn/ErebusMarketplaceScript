184c0aa6ee58cfd125c8912388b5ed9ffb67fd33
King Erebus <squardisk@gmail.com>
2026-06-22 21:10:12 -0400

    Beta v2.0.0 Update Commit

--- config/mail.php
+++ config/mail.php
@@ -2,10 +2,10 @@
 
 /*
  * =========================================================================
- * © 2026 The Erebus Development Team
- * Author: AnonymousUser9183
+ * © 2026 Erebus Labs Inc.
+ * Author: Czar Erebus
  * =========================================================================
- * Laravel 12 Mail Configuration File
+ * Laravel 13 Mail Configuration File
  * =========================================================================
  */
 
